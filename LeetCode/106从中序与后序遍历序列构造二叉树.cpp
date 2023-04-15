baf4097352e6f7de27170449482f9b76894a2f9f
SDoHao <19951122v2@163.com>
2023-04-15 21:50:17 +0800

    106 post+in TreeNode - clear [LeetCode]

--- LeetCode/106从中序与后序遍历序列构造二叉树.cpp
+++ LeetCode/106从中序与后序遍历序列构造二叉树.cpp
@@ -15,22 +15,16 @@ private:
     int posti;
     unordered_map<int,int> index;
 public:
-    TreeNode* make(vector<int>& inorder, vector<int>& postorder,int inst,int ined,int v)
+    TreeNode* make(vector<int>& inorder, vector<int>& postorder,int inst,int ined)
     {
-        std::cout<<posti<<' '<<inst<<' '<<ined<<' '<<v<<std::endl;
-        if(posti < 0||inst <0 ||ined < 0)
-        {
-            std::cout<<"-1"<<std::endl;
-            //return nullptr;
-        }  
         if(inst > ined)
             return nullptr;
         int root_val = postorder[posti];
         int inroot_i = index[root_val];
         posti--;
         TreeNode *root = new TreeNode(root_val);
-        root->right = make(inorder,postorder,inroot_i + 1,ined,1);
-        root->left = make(inorder,postorder,inst,inroot_i - 1,0);
+        root->right = make(inorder,postorder,inroot_i + 1,ined);
+        root->left = make(inorder,postorder,inst,inroot_i - 1);
         
         return root;
     }
@@ -38,10 +32,9 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) 
     {   
         posti  = (int)postorder.size() - 1;
-        std::cout<<posti<<std::endl;
         int idx = 0;
         for(auto &val:inorder)
             index[val] = idx++;
-        return make(inorder,postorder,0,(int)inorder.size() - 1,2);
+        return make(inorder,postorder,0,(int)inorder.size() - 1);
     }
 };
