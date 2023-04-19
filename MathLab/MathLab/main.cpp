6019d6de2ae1755562cb886ce4b7584b5fe28b65
SDoHao <19951122v2@163.com>
2023-04-19 16:15:38 +0800

    BFS maze ++ [MathLab]

--- MathLab/MathLab/main.cpp
+++ MathLab/MathLab/main.cpp
@@ -8,17 +8,91 @@
 
 using namespace mathlab;
 
+using pos = struct pos;
+struct pos
+{
+	int x;
+	int y;
+	pos *pre;
+	//pos(int xx, int yy, pos* p) :x(xx), y(yy), pre(p) {}
+};
+
+void BFS(Matrix maze, int stx, int sty, int edx, int edy)
+{
+	std::queue<pos*> q;
+	std::queue<pos*> path;
+	pos *_pp = new pos{ edx, edy, nullptr };
+	int n = maze.GetColSize() - 1;
+	int m = maze.GetRowSize() - 1;
+	q.push(_pp);
+	while (!q.empty())
+	{
+		_pp = q.front();
+		q.pop();
+		path.push(_pp);
+		maze.Replace(_pp->x, _pp->y, 2);
+		if (_pp->x == stx && _pp->y == sty)
+		{
+			while (!q.empty())
+			{
+				path.push(q.front());
+				q.pop();
+			}
+			break;
+		}
+		// 找相领点并放入队列
+		if (_pp->x > 0 && maze.GetElementValue(_pp->x - 1, _pp->y) == 0)
+		{
+			maze.Replace(_pp->x - 1, _pp->y, 1);
+			pos *_tup = new pos{ _pp->x - 1, _pp->y, _pp };
+			q.push(_tup);
+		}
+		if (_pp->x < n && maze.GetElementValue(_pp->x + 1, _pp->y) == 0)
+		{
+			maze.Replace(_pp->x + 1, _pp->y, 1);
+			pos* _tdown = new pos{ _pp->x + 1, _pp->y, _pp };
+			q.push(_tdown);
+		}
+		if (_pp->y > 0 && maze.GetElementValue(_pp->x , _pp->y- 1) == 0)
+		{
+			maze.Replace(_pp->x , _pp->y - 1, 1);
+			pos* _tleft = new pos{ _pp->x, _pp->y - 1, _pp };
+			q.push(_tleft);
+		}
+		if (_pp->y < m && maze.GetElementValue(_pp->x, _pp->y + 1) == 0)
+		{
+			maze.Replace(_pp->x, _pp->y + 1, 1); 
+			pos* _tright = new pos{ _pp->x, _pp->y + 1, _pp };
+			q.push(_tright);
+		}
+	}
+	//相反向输出路径（测试）
+	while (_pp)
+	{
+		std::cout << '(' << _pp->x << ',' << _pp->y << ')' << std::endl;
+		_pp = _pp->pre;
+	}
+
+	while (!path.empty())
+	{
+		_pp = path.front();
+		path.pop();
+		delete _pp;
+	}
+}
+
 int main()
 {
 	//Draw a 3D model heart
 	//Model_3D _m3d("pointdata.ini");
 	//Draw3D(_m3d, 7, 50, 0, 0, 30);
 
-	Matrix maze1("1.csv");
+	/*Matrix maze1("1.csv");
 	Matrix maze2("2.csv");
 	std::cout << maze1 << std::endl;
 	maze1.ExpandCol(2);
-	std::cout << maze1 << std::endl;
+	std::cout << maze1 << std::endl;*/
+
 	//std::cout << maze2 << std::endl;
 	//std::cout << maze2.Inverse() << std::endl;
 	//std::cout << maze1 *  maze2 << std::endl;
@@ -26,103 +100,14 @@ int main()
 	//std::cout << maze1 /  maze2 << std::endl;
 	//std::cout << maze1.Adjoint() << std::endl;
 
-	////BFS
-	//std::cout << std::endl;
-	//Matrix maze("maze.csv");
-	//std::cout << maze;
+	//BFS
+	std::cout << std::endl;
+	Matrix maze("maze.csv");
+	std::cout << maze;
 
-	//std::queue<POS> Q;
-	//BFS(Q, maze, 0, 0);
+	BFS(maze, 0, 0,9,9);
     return 0;
 }
 
 
 
-/*typedef struct POS POS;
-struct POS
-{
-	size_t x;
-	size_t y;
-	POS* father;
-};
-
-void FindNeighbor(Matrix& visited, POS &node, std::vector<POS>& _pos)
-{
-	size_t r = node.x;
-	size_t c = node.y;
-
-	if (r != 0 && visited.GetElementValue(r - 1, c) == 0)
-	{
-		POS *a = new POS;
-		a->father = &node;
-		a->x = r - 1;
-		a->y = c;
-		_pos.push_back(*a);
-	}
-	if (r != visited.GetRowSize() - 1 && visited.GetElementValue(r + 1, c) == 0)
-	{
-		POS *a = new POS;
-		a->father = &node;
-		a->x = r + 1;
-		a->y = c;
-		_pos.push_back(*a);
-	}
-	if (c != 0 && visited.GetElementValue(r, c - 1) == 0)
-	{
-		POS *a = new POS;
-		a->father = &node;
-		a->x = r;
-		a->y = c - 1;
-		_pos.push_back(*a);
-	}
-	if (c != visited.GetColSize() - 1 && visited.GetElementValue(r, c + 1) == 0)
-	{
-		POS *a = new POS;
-		a->father = &node;
-		a->x = r;
-		a->y = c + 1;
-		_pos.push_back(*a);
-	}
-}
-
-void BFS(std::queue<POS> &_q, Matrix& _mat, size_t x, size_t y)
-{
-	POS _pos;
-	POS _t;
-	std::queue<POS> route;
-	_t.father = nullptr;
-	_pos.x = x;
-	_pos.y = y;
-	_q.push(_pos);
-	_mat.Replace(x, y, 2);
-
-	while (!_q.empty())
-	{
-		_t = _q.front();
-		route.push(_t);
-		_q.pop();
-		std::vector<POS> _vpos;
-		FindNeighbor(_mat, _t, _vpos);
-		std::vector<POS>::iterator _ivp;
-		for (_ivp = _vpos.begin(); _ivp < _vpos.end(); _ivp++)
-		{
-			_mat.Replace(_ivp->x, _ivp->y, 2);
-			std::cout << _mat;
-			if (_ivp->x == 4 && _ivp->y == 4)
-			{
-
-				while (!_q.empty())
-					_q.pop();
-				POS * _tt = &(*_ivp);
-				while (_tt->father)
-				{
-					std::cout << _tt->x << ' ' << _tt->y << std::endl;
-					_tt = _tt->father;
-				}
-				break;
-			}
-			_q.push(*_ivp);
-		}
-	}
-}
-*/
